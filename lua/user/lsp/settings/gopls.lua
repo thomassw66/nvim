ec565cbf61df191f42df169aab70046c1db76446
Thomas Wheeler <thomassw66@gmail.com>
2022-04-23 13:29:00 -0700

    remove plugin/ direcory from git tracking

---
+++ lua/user/lsp/settings/gopls.lua
@@ -0,0 +1,15 @@
+
+local lspconfig = require'lspconfig'
+
+
+
+return {
+	cmd = {"gopls"},
+	filetypes = { "go", "gomod", "gotmpl" },
+	root_dir = 
+		lspconfig.util.root_pattern(
+		  "go.mod",
+		  ".git"
+		),
+	single_file_support = true
+}
