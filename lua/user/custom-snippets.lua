42272443f785c93c4f3c3c9e270a750e8b1f911f
Thomas Wheeler <thomassw66@gmail.com>
2022-04-24 14:31:15 -0700

    added an example loading snippets from file"

--- lua/user/custom-snippets.lua
+++ lua/user/custom-snippets.lua
@@ -5,6 +5,8 @@ local s = luasnip.s
 local i = luasnip.i
 local t = luasnip.t
 
+-- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my-cool-snippets" } })
+
 luasnip.add_snippets("all", {
 	s("union_find", {
 		i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
